ba6963be2c2993455bf959095d5bd3bb1e3c3863
SynxEU <126554945+SynxEU@users.noreply.github.com>
2023-12-18 12:01:52 +0100

    Add project files.

---
+++ Struct/Bil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    public struct Bil
+    {
+        public string Farve;
+        public int Km;
+        public int Hk;
+
+        public Bil(string farve, int km, int hk)
+        {
+            Farve = farve;
+            Km = km;
+            Hk = hk;
+        }
+
+        public void Udskriv()
+        {
+            Console.WriteLine($"Farve: {Farve}, Kilometer: {Km}, Hestekræfter: {Hk}");
+        }
+    }
+}
---
+++ Struct/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Struct
+{
+    class Program
+    {
+        static void Main()
+        {
+            Bil bil1;
+            bil1.Farve = "Blå";
+            bil1.Km = 50000;
+            bil1.Hk = 150;
+
+            Bil bil2 = new Bil("Rød", 70000, 200);
+
+            Console.WriteLine("Bil 1:");
+            bil1.Udskriv();
+
+            Console.WriteLine("\nBil 2:");
+            bil2.Udskriv();
+
+            Bil bil3 = bil1;
+
+            bil1.Farve = "Grøn";
+
+            Console.WriteLine("\nEfter ændring af farven på Bil 1:");
+            Console.WriteLine("Bil 1:");
+            bil1.Udskriv();
+
+            Console.WriteLine("\nBil 3 (kopieret fra Bil 1):");
+            bil3.Udskriv();
+        }
+    }
+
+}
